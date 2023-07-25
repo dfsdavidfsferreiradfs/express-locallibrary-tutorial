2dd8ffed49f9b30100c5de95b3331c35b7cdf213
dfsdavidfsferreiradfs <125693737+dfsdavidfsferreiradfs@users.noreply.github.com>
2023-07-25 10:44:28 -0300

    atv ava em em aula 25/7

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,14 @@ const router = express.Router();
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+router.get("/cool", function (req, res, next) {
+  res.send("you are so cool");
+});
+router.get("/cool/beach", function (req, res, next) {
+  res.send("i love betch");
+});
+router.get("/ifc", function (req, res, next) {
+  res.send("well come ifc");
+});
 
 module.exports = router;
